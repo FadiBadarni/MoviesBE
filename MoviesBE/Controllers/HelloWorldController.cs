1b213c1f74d93f57ca037496aa8ae078231b580d
FadiBadarni <fadybd1@gmail.com>
2024-01-22 15:12:07 +0200

    Initial Commit

---
+++ MoviesBE/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesBE.Controllers
+{
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/message")]
+        public string GetMessage()
+        {
+            return "Hey You!";
+        }
+    }
+}
