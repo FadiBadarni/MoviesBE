faa22103d22c6504bc729ee0aefcdc94d52a6136
FadiBadarni <fadybd1@gmail.com>
2024-02-01 13:38:43 +0200

    Refactor service and repository registrations

--- MoviesBE/Program.cs
+++ MoviesBE/Program.cs
@@ -1,66 +1,25 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using MoviesBE.Middleware;
 using MoviesBE.Repositories.Implementations;
 using MoviesBE.Repositories.Interfaces;
 using MoviesBE.Services;
-using MoviesBE.Services.Database;
-using MoviesBE.Services.Factories;
-using MoviesBE.Services.IMDB;
-using MoviesBE.Services.RT;
-using MoviesBE.Services.TMDB;
 using MoviesBE.Services.UserService;
-using Neo4j.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
-var neo4JConfig = builder.Configuration.GetSection("Neo4j");
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<HttpService>();
-builder.Services.AddScoped<MovieDataService>();
-builder.Services.AddScoped<TmdbApiService>();
-builder.Services.AddScoped<MovieBackdropService>();
-builder.Services.AddScoped<MovieVideoOrganizerService>();
-builder.Services.AddScoped<CrewFilterService>();
-builder.Services.AddScoped<PopularityThresholdService>();
-builder.Services.AddScoped<RatingThresholdService>();
-builder.Services.AddSingleton<Neo4JService>();
-builder.Services.AddSingleton(GraphDatabase.Driver(neo4JConfig["Uri"],
-    AuthTokens.Basic(neo4JConfig["Username"], neo4JConfig["Password"])));
-builder.Services.AddScoped<IMovieRepository, MovieRepository>();
-builder.Services.AddScoped<ICreditsRepository, CreditsRepository>();
-builder.Services.AddScoped<IGenreRepository, GenreRepository>();
-builder.Services.AddScoped<IPCompanyRepository, PCompanyRepository>();
-builder.Services.AddScoped<IPCountryRepository, PCountryRepository>();
-builder.Services.AddScoped<IMLanguageRepository, MLanguageRepository>();
-builder.Services.AddScoped<IMBackdropRepository, MBackdropRepository>();
-builder.Services.AddScoped<IMVideoRepository, MVideoRepository>();
-builder.Services.AddScoped<IMovieCollectionRepository, MovieCollectionRepository>();
-builder.Services.AddScoped<IPaginationTrackerRepository, PaginationTrackerRepository>();
 
-builder.Services.AddScoped<IRatingRepository, RatingRepository>();
-builder.Services.AddSingleton<RatingRepositoryFactory>();
-builder.Services.AddSingleton<MovieRepositoryFactory>();
+// Utilize the extension methods for service registration
+builder.Services.AddNeo4jServices(builder.Configuration);
+builder.Services.AddMovieServices();
+builder.Services.AddAuthenticationServices(builder.Configuration);
 
-builder.Services.AddHostedService<IMDbRatingUpdateService>();
-builder.Services.AddSingleton<IMDbScrapingServiceFactory>();
-builder.Services.AddScoped<IMDbScrapingService>();
-
-builder.Services.AddHostedService<RTRatingUpdateService>();
-builder.Services.AddSingleton<RTScrapingServiceFactory>();
-builder.Services.AddScoped<RTScrapingService>();
-
-builder.Services.AddHostedService<MovieDataCompletionService>();
-
-builder.Services.AddHttpClient<Auth0Client>();
-builder.Services.AddScoped<UserService>();
-builder.Services.AddScoped<AuthenticationService>();
+// Repository and UserService registration
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<UserService>();
 
 // Configure logging
 builder.Logging.AddConsole();
@@ -73,26 +32,6 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod()
             .AllowAnyHeader());
 });
-// JWT Bearer Authentication
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        var auth0Settings = builder.Configuration.GetSection("Auth0");
-        options.Authority = $"https://{auth0Settings["Domain"]}/";
-        options.Audience = auth0Settings["Audience"];
-        options.RequireHttpsMetadata = false;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true
-        };
-    });
 
 var app = builder.Build();
 
---
+++ MoviesBE/Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using MoviesBE.Repositories.Implementations;
+using MoviesBE.Repositories.Interfaces;
+using MoviesBE.Services.Database;
+using MoviesBE.Services.Factories;
+using MoviesBE.Services.IMDB;
+using MoviesBE.Services.RT;
+using MoviesBE.Services.TMDB;
+using MoviesBE.Services.UserService;
+using Neo4j.Driver;
+
+namespace MoviesBE.Services;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddNeo4jServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var neo4JConfig = configuration.GetSection("Neo4j");
+
+        services.AddSingleton(GraphDatabase.Driver(neo4JConfig["Uri"],
+            AuthTokens.Basic(neo4JConfig["Username"], neo4JConfig["Password"])));
+
+        // Repository registrations
+        services.AddScoped<IMovieRepository, MovieRepository>();
+        services.AddScoped<ICreditsRepository, CreditsRepository>();
+        services.AddScoped<IGenreRepository, GenreRepository>();
+        services.AddScoped<IPCompanyRepository, PCompanyRepository>();
+        services.AddScoped<IPCountryRepository, PCountryRepository>();
+        services.AddScoped<IMLanguageRepository, MLanguageRepository>();
+        services.AddScoped<IMBackdropRepository, MBackdropRepository>();
+        services.AddScoped<IMVideoRepository, MVideoRepository>();
+        services.AddScoped<IMovieCollectionRepository, MovieCollectionRepository>();
+        services.AddScoped<IPaginationTrackerRepository, PaginationTrackerRepository>();
+        services.AddScoped<IRatingRepository, RatingRepository>();
+        services.AddSingleton<Neo4JService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddMovieServices(this IServiceCollection services)
+    {
+        // Factories
+        services.AddSingleton<MovieRepositoryFactory>();
+        services.AddSingleton<RatingRepositoryFactory>();
+
+        services.AddScoped<MovieDataService>();
+        services.AddScoped<TmdbApiService>();
+        services.AddScoped<MovieBackdropService>();
+        services.AddScoped<MovieVideoOrganizerService>();
+        services.AddScoped<CrewFilterService>();
+        services.AddScoped<PopularityThresholdService>();
+        services.AddScoped<RatingThresholdService>();
+
+        services.AddHostedService<IMDbRatingUpdateService>();
+        services.AddSingleton<IMDbScrapingServiceFactory>();
+        services.AddScoped<IMDbScrapingService>();
+
+        services.AddHostedService<RTRatingUpdateService>();
+        services.AddSingleton<RTScrapingServiceFactory>();
+        services.AddScoped<RTScrapingService>();
+
+        services.AddHostedService<MovieDataCompletionService>();
+
+        services.AddHttpClient<HttpService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuthenticationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<AuthenticationService>();
+        services.AddHttpClient<Auth0Client>();
+
+        services.AddScoped<UserService.UserService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        var auth0Settings = configuration.GetSection("Auth0");
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = $"https://{auth0Settings["Domain"]}/";
+                options.Audience = auth0Settings["Audience"];
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+        return services;
+    }
+}
